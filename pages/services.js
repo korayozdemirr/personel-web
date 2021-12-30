9a006b3aa7a0be78054d281d59c9c595b4263b53
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2021-12-30 23:27:44 +0300

    Create Service.js

---
+++ pages/services.js
@@ -0,0 +1,17 @@
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+export default function about() {
+  return (
+    <>
+      <section>
+        <div className='container py-d9'>
+          <h1 className='text-secondary text-center'>Servisler</h1>
+        </div>
+      </section>
+      <section className='bg-light'>
+        <div className='container py-d9'>
+          <h2>Yapım aşamasında</h2>
+        </div>
+      </section>
+    </>
+  );
+}

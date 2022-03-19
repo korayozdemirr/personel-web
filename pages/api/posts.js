839e6b4094dd32e2690f3a0f44dd5215036928d7
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-03-19 13:05:51 +0300

    poosts api try cath

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,10 +1,15 @@
 import { db } from '../../firebase/firebase';
 import { collection, getDocs } from 'firebase/firestore';
-const posts = [];
+
 export default async (req, res) => {
-  const querySnapshot = await getDocs(collection(db, 'blogs'));
-  querySnapshot.forEach((doc) => {
-    posts.push(doc.data());
-  });
-  res.status(200).json(posts);
+  const posts = [];
+  try {
+    const querySnapshot = await getDocs(collection(db, 'blogs'));
+    querySnapshot.forEach((doc) => {
+      posts.push(doc.data());
+    });
+    res.status(200).json(posts);
+  } catch (e) {
+    res.status(400).end();
+  }
 };

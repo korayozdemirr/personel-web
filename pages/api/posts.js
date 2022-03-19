49ea4e221fef96fadeeabc17c8a7d9369418db37
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-03-19 12:59:28 +0300

    posts

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,8 +1,7 @@
 import { db } from '../../firebase/firebase';
 import { collection, getDocs } from 'firebase/firestore';
-
+const posts = [];
 export default async (req, res) => {
-  const posts = [];
   const querySnapshot = await getDocs(collection(db, 'blogs'));
   querySnapshot.forEach((doc) => {
     posts.push(doc.data());

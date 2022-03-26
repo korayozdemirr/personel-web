763b84ef1dc85623bc693538dc06b0d2d1ae491c
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-03-26 19:23:14 +0300

    Update posts.js

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,6 +2,15 @@ import { db } from '../../firebase/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 
 const Posts = async (req, res) => {
-  res.status(200).json({ name: 'koray özdemir' });
+  const posts = [];
+  try {
+    const querySnapshot = await getDocs(collection(db, 'blogs'));
+    querySnapshot.forEach((doc) => {
+      posts.push(doc.data());
+    });
+  } catch (e) {
+    res.status(400).end();
+  }
+  res.status(200).json(posts);
 };
 export default Posts;

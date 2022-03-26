573129ee9792260d9ef7a02f36c71563e17572ab
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-03-26 19:15:12 +0300

    Update posts.js

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,15 +2,10 @@ import { db } from '../../firebase/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 
 const Posts = async (req, res) => {
-  const posts = [];
-  try {
-    const querySnapshot = await getDocs(collection(db, 'blogs'));
-    querySnapshot.forEach((doc) => {
-      posts.push(doc.data());
-    });
-    res.status(200).json(posts);
-  } catch (e) {
-    res.status(400).end();
-  }
+  const querySnapshot = await getDocs(collection(db, 'blogs'));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+  res.status(200).json(querySnapshot);
 };
 export default Posts;

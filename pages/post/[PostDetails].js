798fa1f077c36d7df9541d52afe742dd8ed5486b
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-04-11 10:51:40 +0300

    update postdetails page

--- pages/post/[PostDetails].js
+++ pages/post/[PostDetails].js
@@ -1,13 +1,15 @@
-import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import BlogsLeft from '../../components/BlogsLeft';
 import { db } from '../../firebase/firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
-const PostDetails = ({ data }) => {
+import { collection, query, where, getDocs, doc } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import ReactHtmlParser from 'react-html-parser';
+const PostDetails = ({ post }) => {
   return (
     <>
       <section>
         <div className='container py-d9'>
-          <h2 className='text-secondary text-center'>{data[0].title}</h2>
+          <h2 className='text-secondary text-center'>{post[0].title}</h2>
         </div>
       </section>
       <div className='container'>
@@ -15,15 +17,17 @@ const PostDetails = ({ data }) => {
           <div className='col-lg-8'>
             <div className='card' style={{ borderWidth: 0 }}>
               <img
-                src={data[0].imageSrc}
+                src={post[0].imageSrc}
                 width={500}
                 height={500}
                 loading='lazy'
                 className='card-img-top rounded'
               />
               <div className='card-body'>
-                <h3 className='card-title'>{data[0].title}</h3>
-                <p className='card-text'>{data[0].content}</p>
+                <h3 className='card-title'>{post[0].title}</h3>
+                <div className='card-text'>
+                  {ReactHtmlParser(post[0].content)}
+                </div>
               </div>
             </div>
           </div>
@@ -38,24 +42,27 @@ const PostDetails = ({ data }) => {
 
 export default PostDetails;
 export async function getServerSideProps(context) {
-  const posts = [];
-  const q = query(
-    collection(db, 'blogs'),
-    where('slug', '==', context.params.PostDetails)
-  );
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    posts.push(doc.data());
-  });
-
-  const data = posts;
+  console.log(context.query.PostDetails);
+  const post = [];
+  const slug = context.query.PostDetails;
+  try {
+    const q = query(collection(db, 'blogs'), where('slug', '==', slug));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      post.push(doc.data());
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  const data = post;
   if (data.length === 0) {
     return {
       notFound: true,
     };
   }
-
   return {
-    props: { data }, // will be passed to the page component as props
+    props: {
+      post,
+    }, // will be passed to the page component as props
   };
 }

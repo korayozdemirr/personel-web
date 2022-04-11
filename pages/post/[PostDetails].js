28fc9cc96702cc2f79fa276167ac6f5b21ac0f77
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-04-11 17:04:05 +0300

    post details new update

--- pages/post/[PostDetails].js
+++ pages/post/[PostDetails].js
@@ -1,4 +1,5 @@
 import BlogsLeft from '../../components/BlogsLeft';
+import Link from 'next/link';
 import { db } from '../../firebase/firebase';
 import ReactHtmlParser from 'react-html-parser';
 const PostDetails = ({ post }) => {
@@ -16,13 +17,13 @@ const PostDetails = ({ post }) => {
               <img
                 src={post[0].imageSrc}
                 width={300}
-                height={300}
+                height={400}
                 loading='lazy'
                 className='card-img-top rounded'
               />
               <div className='card-body'>
-                <h3 className='card-title'>{post[0].title}</h3>
-                <div className='card-text'>
+                <h3 className='card-title fs-1'>{post[0].title}</h3>
+                <div className='card-text fs-2'>
                   {ReactHtmlParser(post[0].content)}
                 </div>
               </div>
@@ -30,6 +31,15 @@ const PostDetails = ({ post }) => {
           </div>
           <div className='col-lg-4'>
             <BlogsLeft />
+            <p className='fw-normal fs-2 my-4 text-center'>Yeni Postlar</p>
+            <div className='d-flex flex-column  mt-4'>
+              <Link href='/'>
+                <a className='link-dark fs-2 mt-2'>Hello World</a>
+              </Link>
+              <Link href='/'>
+                <a className='link-dark fs-2 mt-2'>Hello World</a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>

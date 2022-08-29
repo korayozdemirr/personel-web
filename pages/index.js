57ad2581ba9c608a64760a671b0100120312a367
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-08-30 00:16:29 +0300

    Update indexjs

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
-import HomeSection from '../components/homesection';
-import AboutSection from '../components/aboutsection';
-import ServiceSection from '../components/servicesection';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import HomeSection from "../components/homesection";
+import AboutSection from "../components/aboutsection";
+import ServiceSection from "../components/servicesection";
+import { useSelector } from "react-redux";
 export default function Home() {
-  const theme = useSelector((state)=>state.theme.value)
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      analytics.logEvent('home page');
-    }
-  });
+  const theme = useSelector((state) => state.theme.value);
+
   return (
     <>
-      <section id='home' className={"bg-"+theme[0]+" "+ "text-"+theme[1]}>
+      <section
+        id="home"
+        className={"bg-" + theme[0] + " " + "text-" + theme[1]}
+      >
         <HomeSection />
       </section>
-      <section id='about' className={"bg-"+theme[0]+" "+ "text-"+theme[1]} >
-        <AboutSection  />
+      <section
+        id="about"
+        className={"bg-" + theme[0] + " " + "text-" + theme[1]}
+      >
+        <AboutSection />
       </section>
-      <section id='service' className={"bg-"+theme[0]+" "+ "text-"+theme[1]} >
+      <section
+        id="service"
+        className={"bg-" + theme[0] + " " + "text-" + theme[1]}
+      >
         <ServiceSection />
       </section>
     </>
